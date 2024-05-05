9472b209932e4609b5e0a39da913e06723f91275
Jack Derifaj <jackd@mac.com>
2024-05-05 12:38:32 -0400

    Initial Commit

---
+++ XcodeCloudDemo/XcodeCloudDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudDemoApp.swift
+//  XcodeCloudDemo
+//
+//  Created by Jack Derifaj on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
